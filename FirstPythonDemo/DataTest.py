2245aaac570fa153877b1a7ec9573712a63cd1d3
梁明辉 <1723153306@qq.com>
2018-11-27 17:52:26 +0800

    测试案例添加

--- FirstPythonDemo/DataTest.py
+++ FirstPythonDemo/DataTest.py
@@ -1,8 +1,16 @@
 # print('%.2f' % 3.1415926)
 # print('%02d.%02d' % (3, 1))
 # print('%.2f' % 3.1415926)
-s1 = int(72);
-s2 = int(85);
-r = float(85-72)/72;
-print("小明成绩提高百分之%.4f"%r)
+# s1 = int(72);
+# s2 = int(85);
+# r = float(85-72)/72;
+# print("小明成绩提高百分之%.4f"%r)
+from OnePlaceQuadratic import quadratic
+from OnePlaceQuadratic import my_function
+# x,y=
+print(quadratic(1,3,-4))
+
+print(my_function())
+
+
 
