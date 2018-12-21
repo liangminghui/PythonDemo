a20041d41f412ada74f340e84b60bebfe1f1642a
梁明辉 <1723153306@qq.com>
2018-12-21 17:45:10 +0800

    登录验证计划>>>

--- FlaskDemo/SqlDemo.py
+++ FlaskDemo/SqlDemo.py
@@ -1,22 +1,37 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-import sqlite3
+import sqlite3;
 conn=sqlite3.connect("../DBDemo/test.db")
-cursor=conn.cursor()
-# modify=cursor.execute("alter table user add password varchar(200)")
-# update =cursor.execute("update user set password='5LKH9UXHiCx45rAXcjM/dw=='")
-select =cursor.execute("select * from user")
-# print(select.fetchall())
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker,relationship
+from sqlalchemy import Column,String,INT,create_engine,ForeignKey
+#定义class对象
+Base=declarative_base()
+class User(Base):
+    __tablename__="user"
+    id=Column(String(20),primary_key=True)
+    name=Column(String(200))
+    score=Column(INT)
+    book=relationship('Book')
 
-def findUserByName(name):
-    #处理sql
-    sql="select * from user where name = '%s'"%name
-    print(sql)
-    select =cursor.execute(sql)
-    print(select.fetchall())
-findUserByName("Adam")
-cursor.close()
-conn.commit()
-conn.close()
+class Book(Base):
+    __tablename__="book"
+    id=Column(String(20),primary_key=True)
+    name=Column(String(200))
+    user_id=Column(String(20),ForeignKey('user.id'))
+#初始化链接
+engine=create_engine('sqlite:///../DBDemo/test.db')
+DBSession=sessionmaker(bind=engine)
+session=DBSession()
 
+#验证登录
+def login(userName,password):
+    book=session.query(Book).filter(Book.user_id=='A-001').all()
+    return book
+    pass
+#获取用户信息
+def getUsersBookInfo(userName):
+    pass
+
+print(login("",""));
