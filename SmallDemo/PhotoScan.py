69715cfcd99179aa90ea0b60c13f0ec25bd4f5ac
梁明辉 <1723153306@qq.com>
2019-01-23 10:42:41 +0800

    图像识别功能完成

--- SmallDemo/PhotoScan.py
+++ SmallDemo/PhotoScan.py
@@ -4,9 +4,10 @@
 # 通用文字识别
 import requests
 import json
+import base64
 client_api_key = "Q72wx1qOCL8pYxtCnEQ2wuFt"
 client_secret_key = "Xh981XBDdgGkqGH3DjuxagjQuQuUFV7H"
-general_basic_url = "https://aip.baidubce.com/rest/2.0/ocr/v1/general_basic"
+accurate_basic_url = "https://aip.baidubce.com/rest/2.0/ocr/v1/accurate_basic"
 def getAccessToken():
     # client_id 为官网获取的AK， client_secret 为官网获取的SK
     host = 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id='+client_api_key+'&client_secret='+client_secret_key
@@ -16,5 +17,20 @@ def getAccessToken():
     result = requests.get(host,headers=headers)
     result = json.loads(result.text)
     return result["access_token"]
-#https://blog.csdn.net/u013421629/article/details/79500336
-print(getAccessToken())
+def AIPhotoWordShiBie():
+    access_token = getAccessToken()
+    headers = {
+        'Content-Type':'application/x-www-form-urlencoded'
+    }
+    with open("6273713-969204f566394e4c.jpg", "rb") as f:
+        image = base64.b64encode(f.read())
+    body = {
+        'image':image
+    }
+    result = requests.post(accurate_basic_url+"?access_token="+access_token,headers=headers,data=body)
+    words_result = json.loads(result.text)["words_result"]
+    for words in words_result:
+        print(words["words"])
+
+if __name__ == '__main__':
+    AIPhotoWordShiBie()
