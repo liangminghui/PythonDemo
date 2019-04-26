87b04a96e757704530a0dfb9acf9e73292640aee
梁明辉 <1723153306@qq.com>
2019-04-26 15:01:56 +0800

    hamc修改

--- SmallDemo/demo/weiFangBus/test.py
+++ SmallDemo/demo/weiFangBus/test.py
@@ -4,10 +4,13 @@
 # 参考地址 https://github.com/hanandjun/weifangbus/blob/develop/lib/utils/requestParamsUtil.dart
 import time
 import random
+import hashlib
+import hmac
+import base64
 
-
-def run():
-    print(">>>")
+# 59485eebe12042cba33e972f77834b6b 聊城
+# 55b73c446e914785862966abf9a29416 潍坊
+app_key = "55b73c446e914785862966abf9a29416"
 
 
 # 获取时间戳
@@ -23,12 +26,13 @@ def _get_random():
 
 # 生成签名密钥
 def _get_sign_key(time_stamp, rand):
-    print(time_stamp)
-    print(rand)
+    message = bytes(str(time_stamp) + str(rand), encoding="utf-8")
+    secret = bytes(app_key, "utf-8")
+    signature = base64.b64encode(hmac.new(secret, message, digestmod=hashlib.sha256).digest())
+    print(signature)
 
 
 if __name__ == '__main__':
     time_stamp = _get_time_stamp()
     rand = _get_random()
-    _get_sign_key(_get_random())
-
+    _get_sign_key(time_stamp, rand)
